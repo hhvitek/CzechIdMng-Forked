adfd85739873caeffe2e46b51d4746907d5b9803
hvitek <hvitek@seznam.cz>
2023-12-18 00:36:28 +0100

    Simple integration test added
    - Added "AbstractIntegrationTest" trying to test constraint in DB - test is not working as expected (ModelMapper)

--- Realization/backend/documents/src/test/java/eu/bcvsolutions/idm/document/rest/impl/DocumentControllerValidationIntegrationTest.java
+++ Realization/backend/documents/src/test/java/eu/bcvsolutions/idm/document/rest/impl/DocumentControllerValidationIntegrationTest.java
@@ -2,14 +2,18 @@ package eu.bcvsolutions.idm.document.rest.impl;
 
 import java.util.UUID;
 
+import javax.validation.ConstraintViolationException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
 
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
 import eu.bcvsolutions.idm.document.TestHelper;
 import eu.bcvsolutions.idm.document.dto.DocumentDto;
+import eu.bcvsolutions.idm.document.dto.filter.DocumentFilter;
 import eu.bcvsolutions.idm.document.service.api.DocumentService;
 import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
 
@@ -29,12 +33,22 @@ public class DocumentControllerValidationIntegrationTest extends AbstractIntegra
 		helper.logout();
 	}
 
-	@Test
+	@Test(expected = ConstraintViolationException.class)
 	public void createDocumentWithNonExistentIdentityTest() {
 		IdmIdentityDto identity = helper.createIdentityOnly();
-		DocumentDto document = helper.getDocument(identity);
-		document.setIdentity(UUID.randomUUID());
 
-		documentService.save(document);
+		DocumentDto document = helper.getDocument(identity);
+		Assert.assertEquals(documentService.count(new DocumentFilter()), 0);
+		DocumentDto savedDocument = documentService.save(document);
+		Assert.assertEquals(documentService.count(new DocumentFilter()), 1);
+
+		// issue with conversion from dto to entity - attribute identity...
+		DocumentDto document2 = helper.getDocument(identity);
+		document2.setIdentity(UUID.randomUUID());
+
+		// should fail on something like sql constrain but fails on model mapper ignoring identity during dto to entity conversion
+		// AbstractReadDtoEntity row 760 return modelMapper.map(dto, getEntityClass(dto));
+		// Maybe its standard behaviour - automatic checking of existence fk identity entity and it simply fails later during entity validation
+		documentService.save(document2);
 	}
 }
